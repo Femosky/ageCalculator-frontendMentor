4921604159cc2ba8cabf665a7b2a9665500675fb
Anitor <anitorabraham@gmail.com>
2023-06-23 23:52:32 +0100

    made some changes on the javascript file

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const allInputs = document.querySelectorAll(".input");
-const inputDay = document.querySelector("#input-day")
-const inputMonth = document.querySelector("#input-month")
-const inputYear = document.querySelector("#input-year")
+const inputDay = document.querySelector("#input-day");
+const inputMonth = document.querySelector("#input-month");
+const inputYear = document.querySelector("#input-year");
 
-// we are going to target the spans now 
-const spanYear = document.querySelector("#span-year")
-const spanMonth = document.querySelector("#span-month")
-const spanDay = document.querySelector("#span-day")
+// we are going to target the spans now
+const spanYear = document.querySelector("#span-year");
+const spanMonth = document.querySelector("#span-month");
+const spanDay = document.querySelector("#span-day");
+
+// getting the birthdate.
 
 // first get the present date using the javascript Date object.
-
 const presentTime = new Date();
 
 // get the present day
@@ -19,36 +20,44 @@ const presentMonth = presentTime.getMonth();
 // get the present year.
 const presentYear = presentTime.getFullYear();
 
-// write event listener that runs a function when input chagnes
+// write event listener that runs a function when input changes.
 
-function getDateDifferenceage(){
-    const dayValue = Number(inputDay.value);
-    const monthValue = Number(inputMonth.value);
-    const yearValue = Number(inputYear.value);
+function getDateDifferenceAge() {
+  const dayValue = Number(inputDay.value);
+  const monthValue = Number(inputMonth.value);
+  const yearValue = Number(inputYear.value);
 
-    // difference between present year and birth year.
-    const diffYear =   presentYear - yearValue
-    const diffMonth =   presentMonth - monthValue
-    const diffDay =   presentDay - dayValue
+  const birthDate = new Date(yearValue, monthValue - 1, dayValue);
 
-    console.log(diffYear, diffMonth, diffDay)
-    // adjust age if months is negative
-    if(diffMonth < 0 || (diffMonth ===0 && diffDay < 0)){
-        diffYear--;
-        diffMonth +=12;
-    }
+  // difference between present year and birth year.
+  const diffYear = presentYear - yearValue;
+  const diffMonth = presentMonth - monthValue;
+  const diffDay = presentDay - dayValue;
 
-    if()
-    spanYear.innerHTML = diffYear
-    spanMonth.innerHTML = diffMonth
-    spanDay.innerHTML = diffDay
-    console.log(typeof(yearValue));
-};
+  if (
+    diffMonth < 0 ||
+    (diffMonth === 0 && presentTime.getDate() < birthDate.getDate())
+  ) {
+    diffYear--;
+    diffMonth += 12;
+  }
+  if (diffDay < 0) {
+    diffMonth--;
+    const prevMonth = new Date(
+      presentTime.getFullYear(),
+      presentTime.getMonth() - 1,
+      0
+    );
+    console.log(prevMonth.getDate());
+    diffDay += prevMonth.getDate();
+  }
 
- 
-
+  spanYear.innerHTML = diffYear;
+  spanMonth.innerHTML = diffMonth;
+  spanDay.innerHTML = diffDay;
+}
 
 // let us use a for loop to go through all the contents of the allInputs array.
-for (let i =0 ; i < allInputs.length; i++){
-    allInputs[i].addEventListener("input",getDateDifferenceage);
+for (let i = 0; i < allInputs.length; i++) {
+  allInputs[i].addEventListener("input", getDateDifferenceAge);
 }
